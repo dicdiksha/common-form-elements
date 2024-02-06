0b0f9f12cd0f127b81cc096ca748ef6e89b282f9
yashveertrigyn <yashveer.negi@trigyn.com>
2024-02-06 10:16:07 +0530

    revert old changes and fix issue with selected option for all tab

--- projects/common-form-elements/src/lib/material-auto-complete/material-auto-complete.component.ts
+++ projects/common-form-elements/src/lib/material-auto-complete/material-auto-complete.component.ts
@@ -98,7 +98,7 @@ export class MaterialAutoCompleteComponent implements OnInit, OnChanges {
       this.selectedOptions.splice(i, 1);
     }
     this.formControlRef.setValue(this.selectedOptions);
-    this.formControlRef.value('');
+
     
   }
 
@@ -153,7 +153,7 @@ export class MaterialAutoCompleteComponent implements OnInit, OnChanges {
         takeUntil(this.dispose$)
       ).subscribe();
     }
-    this.localFrmControl.patchValue('');
+    this.localFrmControl.patchValue(this.default);
     this.setTempValue(this.default);
   }
 
