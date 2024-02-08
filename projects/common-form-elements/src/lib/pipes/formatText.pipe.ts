f87e27fa7bff17c67b07340887883404251cb267
yashveertrigyn <yashveer.negi@trigyn.com>
2024-02-08 17:36:38 +0530

    add new pipe to format text

---
+++ projects/common-form-elements/src/lib/pipes/formatText.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatText'
+})
+export class FormatTextPipe implements PipeTransform {
+  toTitleCase(str) {
+    return str.toLowerCase().replace(/\b\w/g, function (char) {
+      return char.toUpperCase();
+    });
+  }
+
+  transform(text: string): string {
+    if (text.startsWith("state") || text.startsWith("State") || text.startsWith("ut") || text.startsWith("UT")) {
+      return text.split(' ').map(this.toTitleCase).join(' ');
+    }
+    else{
+      return text.toUpperCase();
+    }
+  }
+
+}
--- projects/common-form-elements/src/lib/pipes/pipes.module.ts
+++ projects/common-form-elements/src/lib/pipes/pipes.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { TransposeHtmlPipe } from './transpose-html/transpose-html';
 import { SafeHtmlPipe } from './safe-html/safe-html';
+import { FormatTextPipe } from './formatText.pipe';
 
 @NgModule({
-  declarations: [TransposeHtmlPipe, SafeHtmlPipe],
+  declarations: [TransposeHtmlPipe, SafeHtmlPipe,FormatTextPipe],
   imports: [CommonModule],
-  exports: [TransposeHtmlPipe, SafeHtmlPipe],
+  exports: [TransposeHtmlPipe, SafeHtmlPipe,FormatTextPipe],
   providers: [DatePipe]
 })
 export class PipesModule {
