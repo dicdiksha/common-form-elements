75dd744cb7de4beb750bd233f71ce754173bf600
Tejaswini Giram <tejaswini.giram@trigyn.com>
2024-04-24 11:39:16 +0530

    150772 - Added subjects to capitalize acronyms in the Subject

--- projects/common-form-elements/src/lib/pipes/formatText.pipe.ts
+++ projects/common-form-elements/src/lib/pipes/formatText.pipe.ts
@@ -11,13 +11,15 @@ export class FormatTextPipe implements PipeTransform {
     });
   }
 
-  upperCaseObj = ["igot-health","cbse/ncert","cbse","ncert","cisce","nios","cpd","ut (dnh and dd)", "ict", "nss volunteers", "aas pass (evs)", "craft", "ecce", "evs", "gka", "ict", "tamil (at)", "tamil (bt)","spcc", "nyks","nursing", "nep", "manipuri lairek laisu (meetei mayek)", "looking around (evs)", "kannada (bt)", "ircs"]
-
-  transform(text: string):string{   
-    if(this.upperCaseObj.includes(text)){
+  upperCaseObj = ["igot-health","cbse/ncert","cbse","ncert","cisce","nios","cpd","ut (dnh and dd)", "ict", "nss volunteers", "aas pass (evs)", "craft", "ecce", "evs", "gka", "ict", "tamil(at)", "tamil(bt)","spcc", "nyks","nursing", "nep", "manipuri lairik laisu (meetei mayek)", "looking around (evs)", "kannada(bt)", "ircs"];
+  titleCaseObj = ["cbse training","evs part 1","evs part 2","ict in education"];
+ 
+  transform(text: string):string{  
+    if(this.upperCaseObj.includes(text.toLowerCase())){
       return text.toUpperCase();
-    } else if(text.toLowerCase() ==='cbse training'){
-      return 'CBSE Training'
+    } else if(this.titleCaseObj.includes(text.toLowerCase())){
+      let words = text.split(" ");
+      return words[0].toUpperCase() + " " + this.toTitleCase(words[1] + " "+ (words[2] ?? "")) ;
     } else if( text.startsWith("ut") || text.startsWith("UT")){
       let text1 = text.split(' ')
       let firstPart = text1.shift()
